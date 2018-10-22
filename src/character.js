5f0672acf39d3faab31053539da88ea91ea699f6
EthanFe <sneakcow@gmail.com>
2018-10-22 14:33:29 -0500

    stuff with background

--- src/character.js
+++ src/character.js
@@ -1,24 +1,32 @@
 class Character {
 
   keyPressed(event) {
-    const keyMap = {
+    this.moving = this.keyMap[event.keyCode]
+  }
+
+  keyReleased(event) {
+    const movementDirection = this.keyMap[event.keyCode]
+    if (movementDirection === this.moving)
+      this.moving = null
+  }
+
+  constructor() {
+    this.keyMap = {
       39: "right",//{x: 1, y: 0}, //'right',
       37: "left",//{x: -1, y: 0}, //'left',
       // 40: {x: 0, y: 1}, //'up',
       // 38: {x: 0, y: -1}, //'down'
     }
 
-    this.moving = keyMap[event.keyCode]
-  }
-
-  constructor() {
     this.pos = {x: 0, y: 0}
 
     document.addEventListener("keydown", this.keyPressed.bind(this))
+    document.addEventListener("keyup", this.keyReleased.bind(this))
   }
 
   render() {
     this.move()
+    this.fall()
     const image_path = "./images/kirby.png"
     return `<img class="character" src="${image_path}" style="top: ${this.pos.y}px; left: ${this.pos.x}px"></img>`
   }
@@ -26,9 +34,22 @@ class Character {
   move() {
     const moveSpeed = 15
     if (this.moving === "right") {
-      this.pos = {x: this.pos.x + moveSpeed}
+      let newX = this.pos.x + moveSpeed
+      if (newX > 800)
+        newX = 800
+      this.pos = {x: newX}
+    } else if (this.moving === "left") {
+      let newY = this.pos.x - moveSpeed
+      if (newY < 0)
+        newY = 0
+      this.pos = {x: newY}
     }
-    else if (this.moving === "left")
-      this.pos = {x: this.pos.x - moveSpeed}
+  }
+
+  fall() {
+    const fallAccel = 15;
+    if (this.pos.y > 0) {
+      
+    }
   }
 }
