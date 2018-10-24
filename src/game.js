a2e3fdde935a593246759d17b7214b5943f96bac
github username <github email address>
2018-10-24 15:13:20 -0500

    hearts

--- src/game.js
+++ src/game.js
@@ -75,7 +75,7 @@ class Game {
         }
 
         if (this.chelsea.deleteable()) {
-            document.getElementById('game-view').innerHTML = "<h1 style='margin-top: 0;'> FATALITY!</h1>"
+            document.getElementById('game-view').innerHTML = "<h1 style='margin-top: 0;, text-align: center;'> FATALITY!</h1>"
             document.getElementById('life-stats').innerHTML = ""
         }
     }
